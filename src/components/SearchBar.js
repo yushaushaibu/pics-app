dbef8d801040d9cfeee63d782717933bae93ed7f
yushaushaibu <yushaushaibu1@gmail.com>
2022-08-07 20:17:46 -0700

    searchbar component passes props up to app component to fetch app from app component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,30 @@
 import React from "react";
 
-
 class SearchBar extends React.Component {
-    
-    onInputChange(event) {
-        console.log(event.target.value)
-    }
+  state = { term: "" };
 
-    render() {
-        return (
-            <div className="ui segment">
-                <form className="ui form">
-                    <div className="field">
-                    <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange}/>
-                    </div>
-                </form>
-            </div>
-        );
-    };
+  onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmitt(this.state.term);
+  }
+
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })} /** State updates when input changes */
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
