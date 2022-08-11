9287b612f7369ea3e92957f7efbb0461f5c74ea0
yushaushaibu <yushaushaibu1@gmail.com>
2022-08-10 19:13:31 -0700

    Pic app completion and intro to React ref systems

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
+import './ImageList.css';
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-    // const images = props.images.map(({ id, description, urls}) => {..}) ~ Destructuring image
-    const images = props.images.map((image) => {
-        return <img key={image.id} alt={image.description} src={image.urls.regular} />
-    })
+  // const images = props.images.map(({ id, description, urls }) => {..}) ~ Destructuring image
+  const images = props.images.map((image) => {
+    return (
+      <ImageCard key={image.id} image={image}/>
+    );
+  });
 
-    return <div>{images}</div>
-}
+  return <div className="image-list">{images}</div>;
+};
 
 export default ImageList;
