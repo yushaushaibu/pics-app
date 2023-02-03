ebf23dc547b86244ade48a1f35bb3855acec7886
yushaushaibu <yushaushaibu1@gmail.com>
2023-02-03 00:20:51 +0000

    Controlled and uncontrolled elements / components

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
-    onInputChange = (e) => {
-        console.log(e.target.value)
-    }
+    state = { term: ''};
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange}/>
+            <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value})} />
           </div>
         </form>
       </div>
