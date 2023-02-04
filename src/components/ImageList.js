ba8500c7a236ad5d66e51af4b0699584f397d685
yushaushaibu <yushaushaibu1@gmail.com>
2023-02-04 14:45:08 +0000

    ImageCard component created to render image

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-    const renderedImages = props.images.map(({ id, description, urls }) => {
-        return <img key={id} alt={description} src={urls.regular}/>
-    })
-    return <div>{renderedImages}</div>
+  const renderedImages = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image}/>;
+  });
+  return <div className="image-list">{renderedImages}</div>;
 };
 
 export default ImageList;
