052f5673cf3b9c958bf280ee7b1cfa43acbd8d5f
yushaushaibu <yushaushaibu1@gmail.com>
2023-02-04 22:16:42 +0000

    Pics App V2 implementation

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./ImageList.css";
-import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
   const renderedImages = props.images.map((image) => {
-    return <ImageCard key={image.id} image={image}/>;
+    return <img key={image.id} alt={image.description} src={image.urls.regular}/>;
   });
   return <div className="image-list">{renderedImages}</div>;
 };
