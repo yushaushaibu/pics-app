32317d42960bdbeecb17b3f42ca02999a97841fc
yushaushaibu <yushaushaibu1@gmail.com>
2023-02-05 00:11:18 +0000

    Rendering images with ImageCard component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
   const renderedImages = props.images.map((image) => {
-    return <img key={image.id} alt={image.description} src={image.urls.regular}/>;
+    return <ImageCard key={image.id} image={image} />;
   });
   return <div className="image-list">{renderedImages}</div>;
 };
